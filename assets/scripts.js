d3a7b1bf33d16ce84635d53889eda73f66a7ce20
Tissotkevin <tissot.phi.kevin@laposte.net>
2024-07-07 18:58:41 +0200

    corection 2

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,22 +1,2 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    function onSubmitForm(event) {
-        event.preventDefault();
-    }
-    document.querySelector('.form-container form').addEventListener('submit', onSubmitForm);
-})
+$(document).ready(function(){$('.gallery').mauGallery({columns:{xs:1,sm:2,md:3,lg:3,xl:3},lightBox:!0,lightboxId:'myAwesomeLightbox',showTags:!0,tagsPosition:'top'})});document.addEventListener('DOMContentLoaded',function(){function onSubmitForm(event){event.preventDefault()}
+document.querySelector('.form-container form').addEventListener('submit',onSubmitForm)})
